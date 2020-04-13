1768f45ab7f10eaf89b07d4a7421388be715b809
Andy <andrewryzen@gmail.com>
2020-04-13 14:26:36 -0400

    Big push of all the files

---
+++ Develop/My.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+require("./Routes/apiRoute.js")(app);
+require("./Routes/htmlRoute.js")(app);
+
+app.listen(PORT, function() {
+
+    console.log("App listening on PORT " + PORT);
+  });
